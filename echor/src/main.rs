6a3fe354f10ad683659677af28c698ef59a182ae
sughimura <nxsughi@gmail.com>
2025-05-18 16:06:56 +0900

    tmp

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,9 +1,24 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
     let _matches = App::new("echor")
         .version("0.1.0")
         .author("Noritsugu Sugimura <nxsughi@gmail.com>")
         .about("Rust echo")
+        .args(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .args(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
         .get_matches();
+
+    println!("{:#?}", _matches);
 }
