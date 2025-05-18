68ba657ffdb88b429e9cff24f9277432fc5a4bba
sughimura <nxsughi@gmail.com>
2025-05-18 17:02:14 +0900

    fix-test: introduce Result

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -2,24 +2,29 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn dies_no_args() {
+fn dies_no_args() -> TestResult {
     let mut cmd = Command::cargo_bin("echor").unwrap();
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]
-fn runs() {
+fn runs() -> TestResult {
     let mut cmd = Command::cargo_bin("echor").unwrap();
     cmd.arg("hello").assert().success();
+    Ok(())
 }
 
 #[test]
-fn hello1() {
+fn hello1() -> TestResult {
     let outfile = "tests/expected/hello1.txt";
     let expected = fs::read_to_string(outfile).unwrap();
     let mut cmd = Command::cargo_bin("echor").unwrap();
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
